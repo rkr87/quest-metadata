47ce4f140e5c7a2871aa2293823955c56c3aa14e
Ryan Rowland <ryan.rowland@gmail.com>
2023-12-03 11:41:43 +0000

    refactor(constants.py): relocate VERSION constant to constants.py

--- src/quest_metadata/__version__.py
+++
@@ -1,6 +0,0 @@
-"""
-__version__.py
-
-This module defines the version number for the project.
-"""
-VERSION = "0.0.1"
--- src/quest_metadata/constants/constants.py
+++ src/quest_metadata/constants/constants.py
@@ -1,1 +1,15 @@
+"""
+constants.py
+
+This module defines constant values used throughout the application.
+
+META_DOMAIN (str):
+    The base domain for Meta API requests.
+
+VERSION (str):
+    The version number of the application.
+"""
+
 META_DOMAIN = "https://www.meta.com"
+
+VERSION = "0.0.1"
