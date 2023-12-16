04a6bbccf8f0268c062f2f5a7e8e215fd042e0c4
Ryan Rowland <ryan.rowland@gmail.com>
2023-12-16 13:27:57 +0000

    refactor: consolidated request error handling

--- src/quest_metadata/data/web/github_wrapper.py
+++ src/quest_metadata/data/web/github_wrapper.py
@@ -1,71 +1,89 @@
 """
 github_wrapper.py
 
-This module defines the GitHubWrapper class for fetching external application
-information from GitHub.
+This module defines a GitHubWrapper class that interacts with GitHub to
+fetch app update lists.
 
 GitHubWrapper:
-    A non-instantiable class with a static method for fetching external
-    application information from the specified GitHub URL.
+    A class with a method for fetching external application information
+    from the specified GitHub URL.
 
 Attributes:
-    EXT_APPS (str): The url from which to obtain Github app information.
+    EXT_APPS (str): The URL from which to obtain GitHub app information.
 
 Usage:
-    To use this class, call the static method `get_github_apps()`.
+    To use this class, create an instance by passing an HttpClient instance,
+    and then call the method `get_github_apps()`.
 
-    Example:
+Example:
     ```python
     from github_wrapper import GitHubWrapper
+    from data.web.http_client import HttpClient
+
+    # Create an instance of HttpClient (provide appropriate parameters)
+    http_client = HttpClient()
+
+    # Create an instance of GitHubWrapper
+    github_wrapper = GitHubWrapper(http_client)
 
     # Fetch external applications from GitHub
-    apps = GitHubWrapper.get_github_apps()
+    apps = await github_wrapper.get_github_apps()
     ```
-
-Note: Adjust the usage example based on the actual use case in your code.
 """
-from logging import Logger, getLogger
-
-from aiohttp import ClientResponse, ClientSession
+from aiohttp import ClientResponse
 from typing_extensions import final
 
-from base.non_instantiable import NonInstantiable
+from base.base_class import BaseClass
+from base.singleton import Singleton
 from data.model.github_models import GithubApps
+from data.web.http_client import HttpClient
 
-EXT_APPS: str = 'https://raw.githubusercontent.com/basti564' \
-    + '/LauncherIcons/main/oculus_apps.json'
+EXT_APPS: str = (
+    'https://raw.githubusercontent.com/basti564/LauncherIcons/main/oculus_apps.json'
+)
 
 
 @final
-class GitHubWrapper(NonInstantiable):
+class GitHubWrapper(BaseClass, metaclass=Singleton):  # pyright: ignore[reportMissingTypeArgument]
     """
     GitHubWrapper class for fetching external application information
     from GitHub.
 
     Attributes:
-        _launch_method (str): The method to be used for creating instances.
+        _client (HttpClient): The HttpClient instance for making HTTP requests.
     """
 
-    _launch_method = "get_github_apps"
-
-    @staticmethod
-    async def get_github_apps(http_session: ClientSession) -> GithubApps:
+    def __init__(self, http_client: HttpClient) -> None:
         """
-        Fetches the list of external applications from the specified URL.
+        Initialize the GitHubWrapper instance.
+
+        Args:
+            http_client (HttpClient): An instance of the HttpClient class
+                used for making HTTP requests.
+        """
+        super().__init__()
+        self._logger.info("Initializing GitHub Wrapper")
+        self._client: HttpClient = http_client
+
+    async def get_github_apps(self) -> GithubApps:
+        """
+        Fetch the list of external applications from the specified URL.
 
         Returns:
             GithubApps:
                 A Pydantic model representing a list of external
                 applications retrieved from the GitHub API.
         """
-        logger: Logger = getLogger(__name__)
-        logger.info("Fetching app update list from Github")
+        self._logger.info("Fetching app update list from GitHub")
         headers: dict[str, str] = {'Accept': 'application/json'}
 
-        resp: ClientResponse = await http_session.get(
+        resp: ClientResponse | None = await self._client.get(
             EXT_APPS,
             headers=headers
         )
+
+        if resp is None:
+            return GithubApps()
 
         text = await resp.json(content_type='text/plain; charset=utf-8')
         data: GithubApps = GithubApps.model_validate(text)
--- src/quest_metadata/quest_metadata.py
+++ src/quest_metadata/quest_metadata.py
@@ -1,45 +1,51 @@
-# pyright: reportMissingTypeArgument=false
 """
 quest_metadata.py
-
 This module defines the Application class for managing app data.
 
 Application:
     A class that updates local app data from GitHub and fetches
     additional meta data.
+
+Attributes:
+    _app_manager (AppManager): The local app manager.
+    _meta_wrapper (MetaWrapper): The MetaWrapper for fetching meta data.
+    _github_wrapper (GitHubWrapper): The GitHubWrapper for updating local
+        app data.
 """
+
 import asyncio
 import logging.config
 from datetime import datetime, timedelta
 from typing import final
-
-from aiohttp import ClientSession
 
 from base.base_class import BaseClass
 from base.singleton import Singleton
 from controller.github_updater import GithubUpdater
 from controller.meta_updater import MetaUpdater
 from data.local.app_manager import AppManager
+from data.web.github_wrapper import GitHubWrapper
 from data.web.http_client import HttpClient
 from data.web.meta_cookie import MetaCookie
 from data.web.meta_wrapper import MetaWrapper
 
 
 @final
-class Application(BaseClass, metaclass=Singleton):
+class Application(BaseClass, metaclass=Singleton):  # pyright: ignore[reportMissingTypeArgument]
     """
     Application class for managing app data.
 
     Attributes:
         _app_manager (AppManager): The local app manager.
         _meta_wrapper (MetaWrapper): The MetaWrapper for fetching meta data.
+        _github_wrapper (GitHubWrapper): The GitHubWrapper for updating local
+            appdata.
     """
 
     def __init__(
         self,
         app_manager: AppManager,
         meta_wrapper: MetaWrapper,
-        http_session: ClientSession
+        github_wrapper: GitHubWrapper
     ) -> None:
         """
         Initialize the Application.
@@ -47,11 +53,13 @@ class Application(BaseClass, metaclass=Singleton):
         Args:
             app_manager (AppManager): The local app manager.
             meta_wrapper (MetaWrapper): The MetaWrapper for fetching meta data.
+            github_wrapper (GitHubWrapper): The GitHubWrapper for updating
+            local app data.
         """
         super().__init__()
         self._app_manager: AppManager = app_manager
         self._meta_wrapper: MetaWrapper = meta_wrapper
-        self._http_session: ClientSession = http_session
+        self._github_wrapper: GitHubWrapper = github_wrapper
 
     async def run(self) -> None:
         """
@@ -61,7 +69,7 @@ class Application(BaseClass, metaclass=Singleton):
         additional meta data.
         """
         start: datetime = datetime.now()
-        await GithubUpdater.update(self._app_manager, self._http_session)
+        await GithubUpdater.update(self._app_manager, self._github_wrapper)
         await MetaUpdater.start(self._app_manager, self._meta_wrapper)
         delta: timedelta = datetime.now() - start
         self._logger.info("Completed in %s seconds", delta.seconds)
@@ -75,8 +83,9 @@ async def main() -> None:
     client = HttpClient()
     await client.open_session()
     cookie: str = await MetaCookie.fetch()
-    wrapper = MetaWrapper(cookie, client())
-    app = Application(manager, wrapper, client())
+    meta_wrapper = MetaWrapper(cookie, client)
+    github_wrapper = GitHubWrapper(client)
+    app = Application(manager, meta_wrapper, github_wrapper)
     await app.run()
 
 
