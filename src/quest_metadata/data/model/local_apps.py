f421e73163ece45801c81d19e384f74ba435b421
Ryan Rowland <ryan.rowland@gmail.com>
2023-12-16 16:34:35 +0000

    feat: added logos model

--- src/quest_metadata/data/model/local_apps.py
+++ src/quest_metadata/data/model/local_apps.py
@@ -20,8 +20,18 @@ Usage:
 """
 
 
+from pydantic import Field
+
 from base.base_model import BaseModel
 from base.root_dict_model import RootDictModel
+
+
+class Logos(BaseModel):
+    """
+    Pydantic model for representing logos associated with a local application.
+    """
+    portrait: str | None = None
+    landscape: str | None = None
 
 
 class LocalApp(BaseModel):
@@ -34,6 +44,7 @@ class LocalApp(BaseModel):
     updated: str | None = None
     is_available: bool | None = None
     is_free: bool | None = None
+    logos: Logos | None = Field(default=None, exclude=True)
 
 
 class LocalApps(RootDictModel[str, LocalApp]):
