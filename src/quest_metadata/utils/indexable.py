99f4045998dbebd888c76b4252c5bb3d5c5c09af
Ryan Rowland <ryan.rowland@gmail.com>
2023-12-31 12:28:00 +0000

    feat(utils): extended utils functionality; renamed modules
        - feat(indexable): added indexable utils
        - refactor(list): renamed list_utils -> list
        - feat(list): added ignore_duplicates functionality to flatten_list method
        - feat(list): added safe_list_get method
        - refactor(math): renamed math_utils -> math
        - refactor(string): renamed string_utils -> string

---
+++ src/quest_metadata/utils/indexable.py
@@ -0,0 +1,64 @@
+"""
+Module providing utility functions for safely getting values from indexable
+objects.
+
+Functions:
+- safe_get: Function to safely get values from dictionaries or lists with
+    optional default values.
+"""
+from typing import Any, TypeVar, overload
+
+from utils.list import safe_list_get
+
+_VT = TypeVar("_VT")
+_KT = TypeVar("_KT")
+
+
+@overload
+def safe_get(
+    indexable: dict[_KT, _VT],
+    item: _KT,
+    default: _VT | None = None
+) -> _VT | None:
+    ...
+
+
+@overload
+def safe_get(
+    indexable: list[_VT],
+    item: int,
+    default: _VT | None = None
+) -> _VT | None:
+    ...
+
+
+def safe_get(
+    indexable: dict[Any, _VT] | list[_VT],
+    item: Any,
+    default: _VT | None = None
+) -> _VT | None:
+    """
+    Safely get values from dictionaries or lists with optional default values.
+
+    Args:
+    - indexable (dict[Any, _VT] | list[_VT]): The dictionary or list to extract
+        values from.
+    - item (Any): The key or index to use for extraction.
+    - default (_VT | None, optional): The default value to return if the key or
+        index is not found.
+
+    Returns:
+    - _VT | None: The value found at the specified key or index, or the default
+        value if not found.
+
+    Details:
+    The `safe_get` function is designed to safely extract values from
+    dictionaries or lists, returning the specified default value if the key or
+    index is not found. It also checks the type of the indexable object and
+    redirects the call to `safe_list_get` for lists.
+    """
+    if isinstance(indexable, list) and isinstance(item, int):
+        return safe_list_get(indexable, item, default)
+    if isinstance(indexable, dict):
+        return indexable.get(item, default)
+    return None
