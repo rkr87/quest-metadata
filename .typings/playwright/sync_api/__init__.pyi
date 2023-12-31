094aec229826176974bdc384ff2416a631b5fefc
Ryan Rowland <43894515+rkr87@users.noreply.github.com>
2023-12-31 14:32:16 +0000

    chore: no-ref/delete playwright typings (#67)
    * chore(typings): delete playwright typings

--- .typings/playwright/sync_api/__init__.pyi
+++
@@ -1,9 +0,0 @@
-from playwright._impl._api_structures import Cookie as BrowserCookie
-from playwright.sync_api._context_manager import PlaywrightContextManager
-from playwright.sync_api._generated import BrowserContext
-
-ChromiumBrowserContext = BrowserContext
-Cookie: BrowserCookie
-
-def sync_playwright() -> PlaywrightContextManager:
-    ...
