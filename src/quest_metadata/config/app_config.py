8d1395f8b6817ec2b0ed1bd72fdd183c6758f439
Ryan Rowland <ryan.rowland@gmail.com>
2024-01-01 18:31:29 +0000

    fix(app_config): add missing __init__.py; add missing docstrings

--- src/quest_metadata/config/app_config.py
+++ src/quest_metadata/config/app_config.py
@@ -1,7 +1,26 @@
+"""
+Module providing the application configuration model.
+
+This module defines the `AppConfig` class, a singleton configuration model for
+the application.
+
+Classes:
+- AppConfig: Singleton configuration model for the application.
+
+Usage:
+```python
+
+app_config = AppConfig()
+print(app_config.scrape_locale) > "en_US"
+app_config.scrape_locale = "fr_FR"
+"""
 from base.models import SingletonModel
 
 
 class AppConfig(SingletonModel):
+    """
+    Singleton configuration model for the application.
+    """
     logging_config: str = ".config_app/logging.conf"
     scrape_locale: str = "en_US"
     data_path: str = ".data"
